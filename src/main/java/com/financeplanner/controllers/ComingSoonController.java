9b09cf3387de2240ea39bae3c6d16f4d32549ffc
Ranjeet-Sahu07 <23BET10386@cuchd.in>
2025-11-03 23:48:00 +0530

    Create ComingSoonController.java

---
+++ src/main/java/com/financeplanner/controllers/ComingSoonController.java
@@ -0,0 +1,11 @@
+package com.financeplanner.controllers;
+
+import javafx.scene.control.ToggleButton;
+
+public class ComingSoonController {
+    // TODO: Attach handlers to toggles to show popup: 
+    // "🚧 Coming Soon in Future Update (2025+) 🚀"
+    private void handleToggle(ToggleButton tb) {
+        // placeholder: show alert dialog in implementation
+    }
+}
